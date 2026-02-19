93e9fa2d1ff4e349d199ac1e1128864efd62de60
espositoivan79-commits <esposito.ivan.79@gmail.com>
2026-02-19 19:20:24 +0100

    file modificati da esercizi precedenti

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
-console.log(CICCIO)
-console.log(55)
+let listaStudenti = [];
+class StudenteServices {
+    addStudent(student) {
+        listaStudenti.push(student);
+        console.log(`Studente ${student.name} aggiunto con successo!`);
+    }
+    removeStudent(student) {
+        listaStudenti = listaStudenti.filter((s) => s.id !== student.id);
+        console.log(`Studente ${student.name} rimosso con successo!`);
+    }
+    calcolaMedia(student) {
+        const media = student.voti.reduce((acc, curr) => acc + curr, 0) / student.voti.length;
+        return media;
+    }
+    listaStudentiAttivi() {
+        return listaStudenti.filter((a) => a.isActive == true);
+    }
+    isPromosso(student) {
+        const media = this.calcolaMedia(student);
+        return media > 6;
+    }
+}
+const classe = new StudenteServices();
+const s1 = {
+    id: "1",
+    name: "A",
+    isActive: false,
+    voti: [4, 6, 8]
+};
+const s2 = {
+    id: "2",
+    name: "B",
+    isActive: true,
+    voti: [4, 6, 8]
+};
+classe.addStudent(s1);
+classe.addStudent(s2);
+console.log(classe.calcolaMedia(s1));
+classe.removeStudent(s1);
+console.log(classe.listaStudentiAttivi());
+console.log(classe.isPromosso);
+export {};
+//# sourceMappingURL=primoprogettotypescript.js.map
